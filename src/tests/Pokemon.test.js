b641051281b71b896fe8394a0d76306bd7289178
Matheus Rubi Schreiner <matheusrubisch@gmail.com>
2021-06-10 20:51:41 -0300

    Requisito 6 7

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+test('Tests Pokemon', () => {
+  const pikachu = pokemons[0];
+  const { id, name, type, image, averageWeight: { value, measurementUnit } } = pikachu;
+  const { getByText, getByRole, history } = renderWithRouter(
+    <Pokemon pokemon={ pikachu } isFavorite />,
+  );
+
+  expect(getByText(name)).toBeInTheDocument();
+  expect(getByText(type)).toBeInTheDocument();
+  expect(
+    getByText(`Average weight: ${value} ${measurementUnit}`),
+  ).toBeInTheDocument();
+
+  const pokeImg = getByRole('img', { name: `${name} sprite` });
+  expect(pokeImg).toHaveAttribute('src', image);
+
+  const moreDetailsBtn = getByRole('link', { name: 'More details' });
+  expect(moreDetailsBtn).toHaveAttribute('href', `/pokemons/${id}`);
+
+  userEvent.click(moreDetailsBtn);
+  expect(history.location.pathname).toBe(`/pokemons/${id}`);
+
+  const favoriteStar = getByRole('img', { name: `${name} is marked as favorite` });
+  expect(favoriteStar).toHaveAttribute('src', '/star-icon.svg');
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,57 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const moreDetails = 'More details';
+
+test('Testa se informações detalhadas exibe na tela', () => {
+  const { getByText, getByRole } = renderWithRouter(<App />);
+  const linkDetails = getByText(moreDetails);
+  userEvent.click(linkDetails);
+  const pokeDetails = getByText('Pikachu Details');
+  expect(pokeDetails).toBeInTheDocument();
+  const heading = getByRole('heading', {
+    level: 2,
+    name: /Summary/i,
+  });
+  expect(heading).toBeInTheDocument();
+  const infoText = getByText(/This intelligent Pokémon roasts hard berries/i);
+  expect(infoText).toBeInTheDocument();
+});
+
+test('Testa  se existe mapa na pagina', () => {
+  const { getByText, getByRole, getAllByAltText } = renderWithRouter(<App />);
+  const linkDetails = getByText(moreDetails);
+  userEvent.click(linkDetails);
+  const map = getByRole('heading', {
+    level: 2,
+    name: /Game Locations of Pikachu/i,
+  });
+  expect(map).toBeInTheDocument();
+  const pokeLocation = getAllByAltText('Pikachu location');
+  expect(pokeLocation).toHaveLength(2);
+  expect(pokeLocation[0].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  expect(pokeLocation[1].src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  const location1 = getByText(/Kanto Viridian Forest/i);
+  expect(location1).toBeInTheDocument();
+  const location2 = getByText(/kanto Power Plant/i);
+  expect(location2).toBeInTheDocument();
+});
+test('verifica se o pokemon esta sendo favoritado', () => {
+  const { getByText, getByRole, getByLabelText } = renderWithRouter(<App />);
+  const linkDetails = getByText(moreDetails);
+  userEvent.click(linkDetails);
+
+  const checkBox = getByRole('checkbox');
+  expect(checkBox).toBeInTheDocument();
+
+  userEvent.click(checkBox);
+  expect(checkBox.checked).toBeTruthy();
+
+  userEvent.click(checkBox);
+  expect(checkBox.checked).toBeFalsy();
+
+  const favoriteLabel = getByLabelText('Pokémon favoritado?');
+  expect(favoriteLabel).toBeInTheDocument();
+});
