2ee60f9a4d3ef4f5faf7958ca14db729d80627d1
Matheus Schreiner <matheusrubisch@gmail.com>
2021-07-29 02:16:07 -0300

    desafio 10

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT
+UCASE(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome completo',
+h.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+FROM hr.job_history AS h
+INNER JOIN hr.employees AS e ON h.employee_id = e.employee_id
+WHERE MONTH(h.start_date) <= 3
+ORDER BY 1, 2;
---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+SELECT
+c.ContactName AS 'Nome do contato',
+s.ShipperName AS 'Empresa que fez o envio',
+o.OrderDate AS 'Data de pedido'
+FROM w3schools.orders AS o
+INNER JOIN w3schools.customers AS c ON o.CustomerID = c.CustomerID
+INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+WHERE s.ShipperID <= 2
+ORDER BY 1, 2, 3;
