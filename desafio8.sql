fc0eca3b312563a0e86f2a1bcc81fc8c5d181fbe
Matheus Schreiner <matheusrubisch@gmail.com>
2021-07-29 22:35:22 -0300

    Correção desafio 8 e testar identação

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,13 @@
-SELECT
-c.ContactName AS 'Nome do contato',
-s.ShipperName AS 'Empresa que fez o envio',
-o.OrderDate AS 'Data de pedido'
-FROM w3schools.orders AS o
-INNER JOIN w3schools.customers AS c ON o.CustomerID = c.CustomerID
-INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
-WHERE s.ShipperID <= 2
-ORDER BY 1, 2, 3;
+SELECT 
+    c.ContactName AS 'Nome do contato',
+    s.ShipperName AS 'Empresa que fez o envio',
+    o.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.orders AS o
+        INNER JOIN
+    w3schools.customers AS c ON o.CustomerID = c.CustomerID
+        INNER JOIN
+    w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+WHERE
+    s.ShipperID <= 2
+ORDER BY 1 , 2 , 3;
