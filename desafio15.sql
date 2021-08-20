10673a014132df991704e550cfdfa5d41efa544d
Matheus Schreiner <74193622+MatheusSchreiner@users.noreply.github.com>
2021-08-20 17:22:37 -0300

    Correção do Climate

--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN JOB_TITLE varchar(100))
 BEGIN
 SELECT 
-	ROUND(AVG(e.SALARY),2) as 'Média salarial'
+ROUND(AVG(e.SALARY),2) as 'Média salarial'
 FROM
-	hr.employees as e
+hr.employees as e
 INNER JOIN
-	hr.jobs as j on e.JOB_ID = j.JOB_ID
+hr.jobs as j on e.JOB_ID = j.JOB_ID
 WHERE j.JOB_TITLE = JOB_TITLE;
 END $$
 DELIMITER ;
