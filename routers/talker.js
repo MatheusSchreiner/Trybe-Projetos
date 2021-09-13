43d6d273d5a2160e4b73c855bc05dd7c9f0d3e17
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-13 19:06:37 -0300

    Teste

--- routers/talker.js
+++ routers/talker.js
@@ -3,8 +3,8 @@ const route = require('express').Router();
 const getTalker = require('../middlewares/getTalker');
 const getTalkerById = require('../middlewares/getTalkerById');
 const postTalker = require('../middlewares/postTalker');
-// const putTalker = require('../middlewares/putTalker');
-// const deleteTalker = require('../middlewares/deleteTalker');
+const putTalker = require('../middlewares/putTalker');
+const deleteTalker = require('../middlewares/deleteTalker');
 const searchTalker = require('../middlewares/searchTalker');
 const {
   checkToken, checkName, checkAge, checkTalk, checkWatche, checkRate,
@@ -14,7 +14,7 @@ route.get('/', getTalker);
 route.get('/search', searchTalker);
 route.get('/:id', checkToken, getTalkerById);
 route.post('/', checkToken, checkName, checkAge, checkTalk, checkRate, checkWatche, postTalker);
-// route.put('/:id', checkToken, checkName, checkAge, checkTalk, checkRate, checkWatche, putTalker);
-// route.delete('/:id', checkToken, deleteTalker);
+route.put('/:id', checkToken, checkName, checkAge, checkTalk, checkRate, checkWatche, putTalker);
+route.delete('/:id', checkToken, deleteTalker);
 
 module.exports = route;
