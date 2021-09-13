5e9f1ad2259b13f0f66de87fdfcd976efb0cb2a8
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-13 19:26:57 -0300

    Acho que encontrei o erro

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const talker = require('./routers/talker');
 const login = require('./routers/login');
 
@@ -8,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3001';
+const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
