7ec6976417237aa603f17539a8561ba177e42010
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-13 17:48:23 -0300

    Arrumando requisito 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -2,9 +2,9 @@ const favs = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "G
 
 db.movies.aggregate([
   { $match: { countries: "USA", "tomatoes.viewer.rating": { $gte: 3 }, cast: { $in: favs } } },
-  { $addFields: { num_favs: { $setIntersection: ["$cast", favs] } } },
+  { $addFields: { num_favs: { $size: { $setIntersection: ["$cast", favs] } } } },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
-  { $project: { title: 1, _id: 0, num_favs: 1 } },
+  { $project: { title: 1, _id: 0 } },
   { $skip: 24 },
   { $limit: 1 },
 ]);
