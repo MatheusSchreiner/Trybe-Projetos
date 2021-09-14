48f7505983b47ce3b95e902ada12a37f44347289
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-13 21:58:10 -0300

    Finalizando

--- routers/talker.js
+++ routers/talker.js
@@ -4,17 +4,17 @@ const getTalker = require('../middlewares/getTalker');
 const getTalkerById = require('../middlewares/getTalkerById');
 const postTalker = require('../middlewares/postTalker');
 const putTalker = require('../middlewares/putTalker');
-// const deleteTalker = require('../middlewares/deleteTalker');
-// const searchTalker = require('../middlewares/searchTalker');
+const deleteTalker = require('../middlewares/deleteTalker');
+const searchTalker = require('../middlewares/searchTalker');
 const {
   checkToken, checkName, checkAge, checkTalk, checkWatche, checkRate,
 } = require('../middlewares/validationTalker');
 
 route.get('/', getTalker);
-// route.get('/search', searchTalker);
+route.get('/search', searchTalker);
 route.get('/:id', getTalkerById);
 route.post('/', checkToken, checkName, checkAge, checkTalk, checkRate, checkWatche, postTalker);
 route.put('/:id', checkToken, checkName, checkAge, checkTalk, checkRate, checkWatche, putTalker);
-// route.delete('/:id', checkToken, deleteTalker);
+route.delete('/:id', checkToken, deleteTalker);
 
 module.exports = route;
