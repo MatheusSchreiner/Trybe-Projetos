5dab16868e96916d1360f9a735262d52cb738635
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-13 21:32:52 -0300

    Sei la

--- middlewares/postTalker.js
+++ middlewares/postTalker.js
@@ -1,16 +1,11 @@
 const fs = require('fs').promises;
 
-const postTalker = async (req, res, next) => {
+const postTalker = async (req, res) => {
   const { name, age, talk } = req.body;
-  try {
-    const data = await fs.readFile('talker.json', 'utf8').then((f) => JSON.parse(f));
-    const talker = { name, age, id: data.length + 1, talk };
-    await fs.writeFile('./talker.json', JSON.stringify([...data, talker]));
-    return res.status(201).json(talker);
-  } catch (err) {
-    if (err.code === 'ENOENT') { return res.status(400).json({ ERROR: err }); }
-    next(err);
-  }
+  const data = await fs.readFile('talker.json', 'utf8').then((f) => JSON.parse(f));
+  const talker = { name, age, id: data.length + 1, talk };
+  await fs.writeFile('./talker.json', JSON.stringify([...data, talker]));
+  return res.status(201).json(talker);
 };
 
 module.exports = postTalker;
--- middlewares/token.js
+++ middlewares/token.js
@@ -1,8 +1,8 @@
 const crypto = require('crypto');
 
-const token = (_req, res) => {
-  const number = crypto.randomBytes(8).toString('hex');
-  return res.status(200).json({ number });
+const postToken = (_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  return res.status(200).json({ token });
 };
 
-module.exports = token;
+module.exports = postToken;
