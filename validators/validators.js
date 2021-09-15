04a040caf9d4b9be08c3a974c85523d03ed74aa5
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-15 19:07:35 -0300

    Corrigindo o lint

--- validators/validators.js
+++ validators/validators.js
@@ -4,20 +4,28 @@ const products = require('../models/products');
 const nameProduct = async (name) => {
   const minLetters = 5;
   const code = 'invalid_data';
-  if (typeof name !== 'string') throw err(code, 'O nome precisa ser um texto');
-  if (name.length < minLetters) throw err(code, 'Escreve um nome com mais de 5 letras');
+  if (typeof name !== 'string') {
+    throw err(code, '"name" must be a string');
+  }
+  if (name.length < minLetters) {
+    throw err(code, '"name" length must be at least 5 characters long');
+  } 
 };
 
 const quantityProduct = async (quantity) => {
   const code = 'invalid_data';
-  if (typeof quantity !== 'number') throw err(code, 'A quantidade precisa ser um número');
-  if (quantity.length < 1) throw err(code, 'A quantidade tem que ser maior ou igual a 1');
+  if (typeof quantity !== 'number') {
+    throw err(code, '"quantity" must be a number');
+  }
+  if (quantity < 1) {
+    throw err(code, '"quantity" must be larger than or equal to 1');
+  }
 };
 
 const existByName = async (name) => {
   const code = 'invalid_data';
   const exist = await products.getByName(name);
-  if (exist) throw err(code, 'Este produto já existe');
+  if (exist) throw err(code, 'Product already exists');
 };
 
 module.exports = { nameProduct, quantityProduct, existByName };
