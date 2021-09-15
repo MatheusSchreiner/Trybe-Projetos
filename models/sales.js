ba456f8aa6f9581762eb770f37b2264290640124
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-15 00:31:43 -0300

    Models Sales

--- models/sales.js
+++ models/sales.js
@@ -1,6 +1,22 @@
-const connection = require('./connection');
 const { ObjectId } = require('mongodb');
+const connection = require('./connection');
 
-const collection = () => connection()
+const collection = async () => connection()
   .then((db) => db.collection('sales'));
 
+const create = async (itemSold) => collection()
+  .then((col) => col.insertOne({ itemSold }));
+
+const getById = async ({ id }) => collection()
+  .then((col) => col.findOne({ _id: ObjectId(id) }));
+
+const getAll = async () => collection()
+  .then((col) => col.find().toArray());
+
+const updateById = async ({ id, itemSold }) => collection()
+  .then((col) => col.updateOne({ _id: ObjectId(id) }, { $set: { itemSold } }));
+
+const removeById = async ({ id }) => collection()
+  .then((col) => col.deleteOne({ _id: ObjectId(id) }));
+
+module.exports = { create, getAll, getById, updateById, removeById };
