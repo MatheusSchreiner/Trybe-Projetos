89f9e995d84ae6d67da6d212aa22cf7da04e80ff
Matheus Schreiner <matheusrubisch@gmail.com>
2022-01-19 23:34:07 -0300

    Requisito 2,3 e 5

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if low_index >= high_index:
+        return True
+    
+    if not len(word):
+        return False
+    
+    return (
+        word[low_index] == word[high_index] and
+        is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    )
