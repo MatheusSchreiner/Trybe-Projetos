5fb9175a4db90366e995227a956a4286429a4408
Matheus Rubi Schreiner <matheusrubisch@gmail.com>
2022-01-29 17:07:23 -0300

    requisito 10 e 11

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,32 @@
+import re
+import datetime
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    # https://stackoverflow.com/questions/3483318/performing-regex-queries-with-pymongo
+    all_news = search_news({"title": re.compile(title, re.IGNORECASE)})
+    return [(item["title"], item["url"]) for item in all_news]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.date.fromisoformat(date)
+        all_news = search_news({"timestamp": re.compile(date)})
+        return [(item["title"], item["url"]) for item in all_news]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    all_news = search_news({"sources": re.compile(source, re.IGNORECASE)})
+    return [(item["title"], item["url"]) for item in all_news]
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    all_news = search_news({"categories": re.compile(category, re.IGNORECASE)})
+    return [(item["title"], item["url"]) for item in all_news]
