a41fa90c59b84cf68952401d5e34d88d9fdbecb5
Matheus Rubi Schreiner <matheusrubisch@gmail.com>
2022-02-03 21:42:19 -0300

    first commit

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,14 @@
+import csv
+
+
+def read(path):
+    if not path.endswith(".csv"):
+        raise FileNotFoundError("File not found")
+    with open(path, mode="r") as file:
+        keys = ["costumer", "order", "day"]
+        return list(csv.DictReader(file, delimiter=",", fieldnames=keys))
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    file = read(path_to_file)
+    print(file)
