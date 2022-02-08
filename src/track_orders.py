2f008fa783897203072d699ac16b8ba04e8f00ae
Matheus Rubi Schreiner <matheusrubisch@gmail.com>
2022-02-08 13:45:18 -0300

    Requisito 1, 2 e 3

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,33 @@
+from src.analyze_log import (
+    high_order_costumer,
+    never_goes_costumer,
+    never_order_meal
+)
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"costumer": costumer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return high_order_costumer(self.orders, costumer)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return never_order_meal(self.orders, costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return never_goes_costumer(self.orders, costumer)
 
     def get_busiest_day(self):
-        pass
+        days = [i["day"] for i in self.orders]
+        return max(days, key=days.count)
 
     def get_least_busy_day(self):
-        pass
+        days = [i["day"] for i in self.orders]
+        return min(days, key=days.count)
