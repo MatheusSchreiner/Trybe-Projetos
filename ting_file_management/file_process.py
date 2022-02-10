b2df750e50900e8d605d7910cc124b446abc6508
Matheus Rubi Schreiner <matheusrubisch@gmail.com>
2022-02-10 00:10:13 -0300

    requisito 3'

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    
+    obj = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+
+    if any(i["nome_do_arquivo"] == path_file for i in instance._data):
+        return None
+
+    instance.enqueue(obj)
+    sys.stdout.write(str(obj))
 
 
 def remove(instance):
