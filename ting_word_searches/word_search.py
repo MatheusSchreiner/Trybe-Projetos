4784f390d70ca46beb8070cbfc0a343d17e9e863
Matheus Rubi Schreiner <matheusrubisch@gmail.com>
2022-02-11 00:15:42 -0300

    testando requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,34 @@
+import sys
+
+
+# https://stackoverflow.com/questions/522563/accessing-the-index-in-for-loops
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    file = instance.search(0)
+    instance.dequeue()
+
+    obj = [{
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [{"linha": index + 1}],
+            }
+           for index, line in enumerate(file["linhas_do_arquivo"])
+           if word.lower() in line.lower()
+        ]
+
+    return obj
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    file = instance.search(0)
+    instance.dequeue()
+    
+    obj = [{
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [{"linha": index + 1, "conteudo": line}],
+            }
+            for index, line in enumerate(file["linhas_do_arquivo"])
+            if word.lower() in line.lower()
+        ]
+    
+    return obj
