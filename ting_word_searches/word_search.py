4968757ef5a66ddf5fadb5e73e8bf271aa7520c1
Matheus Rubi Schreiner <matheusrubisch@gmail.com>
2022-02-11 00:20:18 -0300

    corrigindo lint'

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -6,29 +6,26 @@ def exists_word(word, instance):
     file = instance.search(0)
     instance.dequeue()
 
-    obj = [{
-            "palavra": word,
-            "arquivo": file["nome_do_arquivo"],
-            "ocorrencias": [{"linha": index + 1}],
+    return [{
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": [{"linha": index + 1}],
             }
-           for index, line in enumerate(file["linhas_do_arquivo"])
-           if word.lower() in line.lower()
-        ]
-
-    return obj
+            for index, line in enumerate(file["linhas_do_arquivo"])
+            if word.lower() in line.lower()
+            ]
 
 
 def search_by_word(word, instance):
     file = instance.search(0)
     instance.dequeue()
-    
-    obj = [{
-            "palavra": word,
-            "arquivo": file["nome_do_arquivo"],
-            "ocorrencias": [{"linha": index + 1, "conteudo": line}],
+
+    return [{
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": [{"linha": index + 1, "conteudo": line}],
             }
             for index, line in enumerate(file["linhas_do_arquivo"])
             if word.lower() in line.lower()
-        ]
+            ]
     
-    return obj
